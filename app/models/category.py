aacd7d7bd997be12cfe54949f6552ac832261a45
khai bui <khaib22127@gmail.com>
2023-04-07 16:50:36 -0600

    fixed a problem in catagory model

--- app/models/category.py
+++ app/models/category.py
@@ -4,7 +4,7 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 class Category(db.Model):
     __tablename__ = 'categories'
 
-     if environment == "production":
+    if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True),
---
+++ app/seeds/categories.py
@@ -0,0 +1,2 @@
+from app.models import db, category, environment, SCHEMA
+from sqlalchemy.sql import text
