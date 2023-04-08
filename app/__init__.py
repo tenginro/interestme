115c5801ee23ff03e3657786320272199f5d9ffe
Danielle Lei <Danielle.lei2014@gmail.com>
2023-04-07 22:18:10 -0700

    migration done

--- app/__init__.py
+++ app/__init__.py
@@ -5,11 +5,11 @@ from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager
 
-from .app.models import db, User
-from .app.api.user_routes import user_routes
-from .app.api.auth_routes import auth_routes
-from .app.seeds import seed_commands
-from .app.config import Config
+from .models import db, User
+from .api.user_routes import user_routes
+from .api.auth_routes import auth_routes
+from .seeds import seed_commands
+from .config import Config
 
 app = Flask(__name__, static_folder="../react-app/build", static_url_path="/")
 
@@ -17,7 +17,7 @@ app = Flask(__name__, static_folder="../react-app/build", static_url_path="/")
 login = LoginManager(app)
 login.login_view = "auth.unauthorized"
 
-db.init_app(app)
+# db.init_app(app)
 
 
 @login.user_loader
