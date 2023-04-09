20ff4aa2cbba49c9828e6a4a1e29611a6af43569
tengzhong luo <108156588+tenginro@users.noreply.github.com>
2023-04-09 19:23:44 -0400

    adding some css to nav bar

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import ProfileButton from './ProfileButton';
-import './Navigation.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-function Navigation({ isLoaded }){
-	const sessionUser = useSelector(state => state.session.user);
+import ProfileButton from "./ProfileButton";
+import "./Navigation.css";
 
-	return (
-		<ul>
-			<li>
-				<NavLink exact to="/pins">Home</NavLink>
-			</li>
-			{isLoaded && (
-				<li>
-					<ProfileButton user={sessionUser} />
-				</li>
-			)}
-		</ul>
-	);
+function Navigation({ isLoaded }) {
+  const sessionUser = useSelector((state) => state.session.user);
+
+  return (
+    <ul className="nav ul">
+      <li className="home li">
+        <NavLink exact to="/pins">
+          <div className="logoLine">
+            <img className="logo" src={require("./icon.png")} alt="logo"></img>
+            Tinterest
+          </div>
+        </NavLink>
+      </li>
+      {isLoaded && (
+        <li className="profile li">
+          <ProfileButton user={sessionUser} />
+        </li>
+      )}
+    </ul>
+  );
 }
 
 export default Navigation;
