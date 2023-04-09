4689d6fcb9dfcff159aaa0fb860825059330e23a
Danielle Lei <Danielle.lei2014@gmail.com>
2023-04-08 17:41:15 -0700

    currentPinComp

---
+++ react-app/src/components/ManagePins/index.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react";
+
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { getUserPins } from "../../store/pin";
+
+function CurrentPins() {
+  const dispatch = useDispatch();
+  const pinsObj = useSelector((state) => state.pins.allPins);
+
+  useEffect(() => {
+    dispatch(getUserPins());
+  }, [dispatch]);
+  
+  if (!pinsObj) return null;
+  const pins = Object.values(pinsObj);
+
+  return (
+    <div>
+      <nav>
+        {pins.map((pin) => (
+          <div>
+            <NavLink key={pin.id} to={`/pins/${pin.id}`}>
+              <img src={pin.url} alt={pin.name} />
+            </NavLink>
+            <button>Save</button>
+          </div>
+        ))}
+      </nav>
+    </div>
+  );
+}
+
+export default CurrentPins;
