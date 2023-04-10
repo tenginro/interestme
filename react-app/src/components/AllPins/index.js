6f48d293b1bd16817be5a8b4d63951b0cbf358c6
Danielle Lei <Danielle.lei2014@gmail.com>
2023-04-10 16:15:46 -0700

    save

--- react-app/src/components/AllPins/index.js
+++ react-app/src/components/AllPins/index.js
@@ -1,19 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState } from "react";
 
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { actionClearPins, getAllPins } from "../../store/pin";
+import * as pinsAction from "../../store/pin";
+
 function AllPins() {
   const dispatch = useDispatch();
   const pinsObj = useSelector((state) => state.pins.allPins);
-  
+  const boardObj = useSelector((state) => state.boards.allBoards);
+  const [board, setBoard] = useState('profile');
+  const [save, setSave] = useState(false);
+
   useEffect(() => {
     dispatch(getAllPins());
     return () => dispatch(actionClearPins());
   }, [dispatch]);
 
+
   if (!pinsObj) return null;
   const pins = Object.values(pinsObj);
+
+  
+  const boards = Object.values(boardObj)
+  
+  
 
   return (
     <div>
@@ -23,7 +34,30 @@ function AllPins() {
             <NavLink key={pin.id} to={`/pins/${pin.id}`}>
               <img src={pin.url} alt={pin.name} />
             </NavLink>
-            <button>Save</button>
+            <select
+              onChange={(e) => {
+                setBoard(e.target.value);
+              }}
+              value={board}
+              name="board"
+              placeholder="Choose a board"
+            >
+              <option value="Profile">Profile</option>
+              {boards.map((c) => (
+                <option value={c}>{c}</option>
+              ))}
+            </select>
+            {
+              save ? <button onClick = {async(e)=> {
+              e.preventDefault();
+              const SaveRes = await dispatch(pinsAction.unSavePinThunk(pin, board))}
+              }>Saved</button> : 
+              <button onClick={ async(e)=> {
+              e.preventDefault();
+              const SaveRes = await dispatch(pinsAction.savePinThunk(pin, board))
+            }
+            }>Save</button>
+            }
           </div>
         ))}
       </nav>
