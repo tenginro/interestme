357274e5eae48b05a77088a64ca46a41ff154e7e
khai bui <khaib22127@gmail.com>
2023-04-10 17:46:33 -0600

    create board and edit board -waiting

--- react-app/src/components/CreateBoard/index.js
+++ react-app/src/components/CreateBoard/index.js
@@ -1,5 +1,17 @@
-function CreateBoard() {
-    return (null)
+import BoardForm from "../BoardForm";
+
+const CreateBoard = () => {
+    const newBoard = {
+        name: "",
+        description: ""
+    }
+    return (<div>
+        <BoardForm
+    newBoard={newBoard}
+    formType="Create board"
+    submitType="Create"
+        />
+    </div>)
 }
 
 export default CreateBoard;
--- react-app/src/components/EditBoard/index.js
+++ react-app/src/components/EditBoard/index.js
@@ -1,5 +1,24 @@
-function EditBoard() {
-    return (null)
+import BoardForm from "../BoardForm";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import * as boardsActions from "../../store/board";
+
+const EditBoard = ()=> {
+ const dispatch = useDispatch();
+
+const user_board = useSelector(state=> state)
+console.log("user board:::===> ", user_board)
+
+useEffect(()=> {
+    dispatch(boardsActions.getUserBoards())
+},[dispatch])
+
+  return (
+    <div>
+        <h1>Hello From edit</h1>
+      {/* <BoardForm formType="Create board" submitType="Create" /> */}
+    </div>
+  );
 }
 
 export default EditBoard;
