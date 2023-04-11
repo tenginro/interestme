7c176802c192dc24df734ea6dd07164171799893
khai bui <khaib22127@gmail.com>
2023-04-11 10:47:28 -0600

    added edit and delete button for single board

--- react-app/src/components/BoardDetails/index.js
+++ react-app/src/components/BoardDetails/index.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { getBoardDetail } from '../../store/board'
 import PinGalleryCard from '../PinGalleryCard'
+import {useModal} from "../../context/Modal"
 
 function BoardDetails() {
     // Extract parameter variables from parameter object
@@ -12,7 +13,7 @@ function BoardDetails() {
 
     // Create dispatch method
     const dispatch = useDispatch()
-
+    const { setModalContent } = useModal();
     // Subscribe to single board slice of state
     const board = useSelector(state => state.boards.singleBoard)
 
@@ -20,25 +21,37 @@ function BoardDetails() {
     const currentUser = useSelector(state => state.session.user)
 
     // Upon component render, dispatch the action to load the single board into the redux store for retrieval
-    // let board 
+    // let board
     useEffect(() => {
         dispatch(getBoardDetail(boardId))
     }, [dispatch, boardId])
-    
+
     if(!board) return null
 
     return (
-        <div className='whole-container'>
-            <h1>{board.name}</h1>
-            <img src={board.board_cover} />
-            <p>Secret Board? {board.secret}</p>
-            <h3>Pin Gallery</h3>
-            <p>{board.Pins?.length} pins</p>
-            {board.Pins && (board.Pins.map(pin => (
-                <PinGalleryCard key={pin.id} pin={pin} />
-            )))}
+      <div className="whole-container">
+        <h1>{board.name}</h1>
+        <img src={board.board_cover} alt="board-cover-pic" />
+        <p>Secret Board? {board.secret}</p>
+
+        <div className="update-delete-board-btn">
+          <span>
+            <button className='edit-board-btn'>
+              Edit
+            </button>
+          </span>
+          <span>
+            <button className="edit-user-board-btnn">
+              DELETE
+            </button>
+          </span>
         </div>
-    )
+        <h3>Pin Gallery</h3>
+        <p>{board.Pins?.length} pins</p>
+        {board.Pins &&
+          board.Pins.map((pin) => <PinGalleryCard key={pin.id} pin={pin} />)}
+      </div>
+    );
 }
 
 export default BoardDetails
