8ff8cfcfdf24386483598d6929ee1b32e8adff96
Danielle Lei <Danielle.lei2014@gmail.com>
2023-04-11 00:15:53 -0700

    display author name

--- react-app/src/components/SinglePin/index.js
+++ react-app/src/components/SinglePin/index.js
@@ -7,20 +7,30 @@ const Pin = () => {
   const { pinId } = useParams();
   const dispatch = useDispatch();
   const pin = useSelector((state) => state.pins.singlePin);
+  console.log('pin inside component', pin)
+  const author = pin.User;
+  console.log('author', author);
+  const authorUsername = author["username"];
+  console.log('authorusername', authorUsername)
 
   useEffect(() => {
     dispatch(getPinDetail(pinId));
-    return () => dispatch(actionClearPin());
+    // return () => dispatch(actionClearPin());
   }, [dispatch, pinId]);
 
   return (
     <div>
-      <div>
+      <div className="leftSide">
         <img src={pin.url} alt="pin.url" />
       </div>
-      <div>
+      <div className="rightSide">
+        <button>Save</button>
         <h2>{pin.name}</h2>
         <p>{pin.description}</p>
+        <div>
+          <h4>{pin.User.username}</h4>
+          <button>Follow</button>
+        </div>
       </div>
     </div>
   );
