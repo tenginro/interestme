084f7b897a525e2d360064c8b1ea2eaea38770a2
tengzhong luo <108156588+tenginro@users.noreply.github.com>
2023-04-10 23:17:10 -0400

    update get following for any user

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, jsonify
-from flask_login import login_required
+from flask_login import login_required, current_user
 from ..models import User
 from sqlalchemy.orm import joinedload, subqueryload
+from sqlalchemy.sql import exists
 
 user_routes = Blueprint('users', __name__)
 
@@ -22,10 +23,30 @@ def user(id):
     """
     Query for a user by id and returns that user in a dictionary
     """
-    user = User.query.options(joinedload(User.following)).get(id)
+    # user = User.query.options(joinedload(User.following)).get(id)
     
+    this_user=User.query.get(id)
+    following = [followingUser.to_dict() for followingUser in this_user.following]
+    
+    allUsers = User.query.all()
+    followers = [user for user in allUsers if this_user in user.following]
+
+    return {**this_user.to_dict(), "following": following, "followers": [follower.to_dict() for follower in followers]}
+
+
+@user_routes.route('/<int:id>/follow', methods=["POST"])
+@login_required
+def following(id):
+    user_to_follow = User.query.options(joinedload(User.following)).get(id)
+    
+    currentUser = current_user.to_dict()
+    user = User.query.options(joinedload(User.following)).get(currentUser.id)
     following = [followingUser.to_dict() for followingUser in user.following]
+    user.following.append(user_to_follow.to_dict())
     
+    db.session.commit()
+    user = User.query.options(joinedload(User.following)).get(currentUser.id)
+    following = [followingUser.to_dict() for followingUser in user.following]
     followers = User.query.filter(User.following.any(id=id)).all()
-    
+
     return {**user.to_dict(), "following": following, "followers": [follower.to_dict() for follower in followers]}
