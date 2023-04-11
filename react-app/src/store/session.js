eb96ebfaa9be82e28651e3c023e4bb840a56a733
Danielle Lei <Danielle.lei2014@gmail.com>
2023-04-10 23:49:14 -0700

    follow thunks added

--- react-app/src/store/session.js
+++ react-app/src/store/session.js
@@ -1,6 +1,10 @@
 // constants
 const SET_USER = "session/SET_USER";
 const REMOVE_USER = "session/REMOVE_USER";
+const GET_USER_FOLLOWING = "session/GET_USER_FOLLOWING"
+const GET_USER_FOLLOWERS = "session/GET_USER_FOLLOWERS"
+// const ADD_USER_FOLLOW = "session/ADD_USER_FOLLOW"
+// const REMOVE_USER_FOLLOW = 'session/REMOVE_USER_FOLLOW'
 
 const setUser = (user) => ({
   type: SET_USER,
@@ -11,7 +15,70 @@ const removeUser = () => ({
   type: REMOVE_USER,
 });
 
+const actionGetFollowing = (following) => ({
+  type: GET_USER_FOLLOWING,
+  payload: following
+})
+
+const actionGetFollowers = (follower) => ({
+  type: GET_USER_FOLLOWERS,
+  payload: follower
+})
+
+// const actionAddFollow = (user) => ({
+//   type: ADD_USER_FOLLOW,
+//   payload: user
+// })
+
+// const actionRemoveFollow = () => ({
+//   type: REMOVE_USER_FOLLOW
+// })
+
 const initialState = { user: null };
+
+export const getFollowingThunk = (user) => async (dispatch) => {
+  const response = await fetch(`/${user.id}`);
+  if(response.ok) {
+    const userRes = await response.json();
+    const following = userRes.following;
+    await dispatch(actionGetFollowing(following));
+    return following;
+  }
+}
+
+export const getFollowerThunk = (user) => async (dispatch) => {
+  const response = await fetch(`/${user.id}`);
+  if(response.ok) {
+    const userRes = await response.json();
+    const followers = userRes.followers;
+    await dispatch(actionGetFollowers(followers));
+    return followers;
+  }
+}
+
+export const addFollowThunk = (user, followingId) => async (dispatch) => {
+  const response = await fetch(`/${followingId}/follow`, {
+    method:'POST',
+  });
+  if(response.ok) {
+    const userRes = await response.json();
+    const following = userRes.following;
+    await dispatch(actionGetFollowing(following));
+    return following;
+  }
+}
+
+export const removeFollowThunk = (user, followingId) => async (dispatch) => {
+  const response = await fetch(`/${followingId}/follow`, {
+    method:'DELETE',
+  });
+  if(response.ok) {
+    const userRes = await response.json();
+    const following = userRes.following;
+    await dispatch(actionGetFollowing(following));
+    return following;
+  }
+}
 
 export const authenticate = () => async (dispatch) => {
   const response = await fetch("/api/auth/", {
@@ -106,6 +173,23 @@ export default function reducer(state = initialState, action) {
       return { user: action.payload };
     case REMOVE_USER:
       return { user: null };
+    case GET_USER_FOLLOWING:
+      const allFollowing = {};
+      action.following.forEach((f)=>{
+        allFollowing[f.id] = f;
+      })
+      return { ...state, user: {...allFollowing}};
+    case GET_USER_FOLLOWERS:
+      const allFollowers = {};
+      action.followers.forEach((f)=>{
+        allFollowers[f.id] = f;
+      })
+      return { ...state, user: {...allFollowers}};
+    // case ADD_USER_FOLLOW:
+    //   return {
+    //     ...state,
+    //     allFollowing: {...state.users, }
+    //   }
     default:
       return state;
   }
