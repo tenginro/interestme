6927ba97cf3cc49b482ff385b3be5581f40160d0
tengzhong luo <108156588+tenginro@users.noreply.github.com>
2023-04-11 22:44:17 -0400

    fix savepins minor change

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -5,31 +5,43 @@ import logo from "../LandingPage/Assets/icon.png";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
 
-function Navigation({ isLoaded }){
-	const sessionUser = useSelector(state => state.session.user);
+function Navigation({ isLoaded }) {
+  const sessionUser = useSelector((state) => state.session.user);
 
-	const clickCreate = (e) => {
-		e.preventDefault();
-	}
+  const clickCreate = (e) => {
+    e.preventDefault();
+  };
 
-	return (
+  return (
     <ul className="nav ul">
       <li className="home li">
-        <NavLink exact to="/pins">
-          <div className="logoLine">
-            <img className="logo" src={logo} alt="logo" />
-            Tinterest
-          </div>
-        </NavLink>
+        {sessionUser?.id && (
+          <NavLink exact to="/pins">
+            <div className="logoLine">
+              <img className="logo" src={logo} alt="logo" />
+              Tinterest
+            </div>
+          </NavLink>
+        )}
+        {!sessionUser?.id && (
+          <NavLink exact to="/pins">
+            <div className="logoLine">
+              <img className="logo" src={logo} alt="logo" />
+              Tinterest
+            </div>
+          </NavLink>
+        )}
       </li>
-	  <div>
-				{sessionUser ?
-				(
-					<button onClick={clickCreate}>
-						<NavLink exact to='/pins/new'> Create </NavLink>
-					</button>
-				) : (null)}
-			</div>
+      <div>
+        {sessionUser ? (
+          <button onClick={clickCreate}>
+            <NavLink exact to="/pins/new">
+              {" "}
+              Create{" "}
+            </NavLink>
+          </button>
+        ) : null}
+      </div>
       {isLoaded && (
         <li className="profile li">
           <ProfileButton user={sessionUser} />
