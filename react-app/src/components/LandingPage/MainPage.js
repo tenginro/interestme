081ce7769e4c74a8e1109e9b568f5643b8807f5e
khai bui <khaib22127@gmail.com>
2023-04-11 18:55:56 -0600

    landing page is still in working progress

--- react-app/src/components/LandingPage/MainPage.js
+++ react-app/src/components/LandingPage/MainPage.js
@@ -1,17 +1,20 @@
 import MainPage1 from "./MainPage1";
 import MainPage2 from "./MainPage2";
 import MainPage3 from "./MainPage3";
-import { Scrollbar } from "smooth-scrollbar-react";
 import "./MainPage.css";
 
 const MainPage = () => {
   return (
     <div className="main_page-container">
+      <div>
       <h1>HELLO FROM MAIN PAGE</h1>
-
-      <MainPage1 />
+      </div>
+      <div className="page2-image"></div>
+      <MainPage1 src={`./Assets/image1.jpeg`} layout={'fill'}/>
       <MainPage2 />
-      <MainPage3 />
+
+        <MainPage3 />
+
     </div>
   );
 };
--- react-app/src/components/LandingPage/MainPage3.js
+++ react-app/src/components/LandingPage/MainPage3.js
@@ -1,10 +1,17 @@
 import image3 from './Assets/image3.jpeg'
 import SignupFormPage from '../SignupFormPage';
 const MainPage3 = () => {
+     const myStyle = {
+       backgroundImage: `url(${image3})`,
+       height: "100vh",
+       width: "100%",
+       backgroundSize: "cover",
+       backgroundRepeat: "no-repeat",
+     };
    return (
-     <div className="main-page3-container" style={{backgroundImage: "image3"}}>
-       <img className="page3-image" src={image3} alt="scenic-pic-3" />
-{/* <SignupFormPage/> */}
+     <div className="main-page3-container" style={myStyle}>
+       {/* <img className="page3-image" src={image3} alt="scenic-pic-3" /> */}
+<SignupFormPage/>
      </div>
    );
 };
