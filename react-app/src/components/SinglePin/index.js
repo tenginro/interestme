b5b493b655692c8ae137f63a00e30a365cf34d6b
tengzhong luo <108156588+tenginro@users.noreply.github.com>
2023-04-12 16:38:11 -0400

    minor change

--- react-app/src/components/SinglePin/index.js
+++ react-app/src/components/SinglePin/index.js
@@ -6,8 +6,6 @@ import * as sessionAction from "../../store/session";
 import * as pinsAction from "../../store/pin";
 import { whichBoard, isSaved } from "../AllPins/PinIndexItem";
 
-
-
 const Pin = () => {
   const { pinId } = useParams();
   const dispatch = useDispatch();
@@ -17,8 +15,8 @@ const Pin = () => {
   const [board, setBoard] = useState(whichBoard(pin, user));
   const [save, setSave] = useState(false);
 
-  console.log('inside single Pin user.id', user.id);
-  console.log('inside single Pin pin.user_saved', pin.user_saved)
+  console.log("inside single Pin user.id", user.id);
+  console.log("inside single Pin pin.user_saved", pin.user_saved);
 
   const userBoards = user?.boards || [];
 
@@ -46,10 +44,10 @@ const Pin = () => {
   }, [dispatch, pinId, save]);
   //when hitting save button, it will reload the whole page
 
-  console.log('above loading', pin)
-  console.log('above loading pin.User', pin?.User)
-  console.log('user.id', user?.id)
-  console.log('pin.id', pin?.id)
+  console.log("above loading", pin);
+  console.log("above loading pin.User", pin?.User);
+  console.log("user.id", user?.id);
+  console.log("pin.id", pin?.id);
   // if(!pin) return null;
   if (!user.id || !pin.id) return <div>Loading</div>;
 
@@ -60,49 +58,50 @@ const Pin = () => {
       </div>
       <div className="rightSide">
         <select
-        onChange={(e) => {
-          changeBoard(Number(e.target.value));
-        }}
-        value={changingBoardId}
-        name="board"
-        placeholder="Choose a board"
-      >
-        <option value="0">Profile</option>
-        {userBoards.length > 0 &&
-          userBoards.map((c) => (
-            <option key={c.id} value={c.id}>
-              {c.name}
-            </option>
-          ))}
-      </select>
-      {isSaved(pin, user) ? (
-        <button
-          onClick={async (e) => {
-            e.preventDefault();
-            changeBoard(0);
-            await dispatch(pinsAction.unSavePinThunk(pin)).then(() => {
-              if (save === false) setSave(true);
-              else setSave(false);
-            });
-            
+          onChange={(e) => {
+            changeBoard(Number(e.target.value));
           }}
+          value={changingBoardId}
+          name="board"
+          placeholder="Choose a board"
         >
-          Unsave
-        </button>
-      ) : (
-        <button
-          onClick={async (e) => {
-            e.preventDefault();
-            changeBoard(changingBoardId);
-            await dispatch(pinsAction.savePinThunk(pin, changingBoardId)).then(() => {
-              if (save === false) setSave(true);
-              else setSave(false);
-            });
-          }}
-        >
-          Save
-        </button>
-      )}
+          <option value="0">Profile</option>
+          {userBoards.length > 0 &&
+            userBoards.map((c) => (
+              <option key={c.id} value={c.id}>
+                {c.name}
+              </option>
+            ))}
+        </select>
+        {isSaved(pin, user) ? (
+          <button
+            onClick={async (e) => {
+              e.preventDefault();
+              changeBoard(0);
+              await dispatch(pinsAction.unSavePinThunk(pin)).then(() => {
+                if (save === false) setSave(true);
+                else setSave(false);
+              });
+            }}
+          >
+            Unsave
+          </button>
+        ) : (
+          <button
+            onClick={async (e) => {
+              e.preventDefault();
+              changeBoard(changingBoardId);
+              await dispatch(
+                pinsAction.savePinThunk(pin, changingBoardId)
+              ).then(() => {
+                if (save === false) setSave(true);
+                else setSave(false);
+              });
+            }}
+          >
+            Save
+          </button>
+        )}
         <h2>{pin.name}</h2>
         <p>{pin.description}</p>
         <div>
@@ -111,7 +110,6 @@ const Pin = () => {
             <button
               onClick={async (e) => {
                 e.preventDefault();
-                console.log("click to unfollow");
                 await dispatch(
                   sessionAction.removeFollowThunk(user, pin.user_id)
                 );
@@ -124,7 +122,6 @@ const Pin = () => {
             <button
               onClick={async (e) => {
                 e.preventDefault();
-                console.log("click to follow");
                 await dispatch(sessionAction.addFollowThunk(pin.user_id));
                 setFollow(true);
               }}
