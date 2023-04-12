97979c1bdd44504aba6c212e811869c6a0def205
khai bui <khaib22127@gmail.com>
2023-04-11 20:06:17 -0600

    fixing css

--- react-app/src/components/DeleteBoard/index.js
+++ react-app/src/components/DeleteBoard/index.js
@@ -1,5 +1,47 @@
-function DeleteBoard() {
-    return (null)
-}
+import { useModal } from "../../context/Modal";
+import * as boardsActions from "../../store/board";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import './DeleteBoard.css'
+
+const DeleteBoard = ({ board }) => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const { closeModal } = useModal();
+
+  const deleteBoardClick = (e) => {
+    e.preventDefault();
+    dispatch(boardsActions.deleteBoard(board.id)).then(() => {
+      history.push("/boards/current");
+      closeModal();
+    });
+  };
+
+  const cancelSubmit = () => {
+    closeModal();
+  };
+
+  return (
+    <div className="container">
+      <div className="title_text">Confirm Delete</div>
+      <div className="confirmation-text">
+        Are you sure you want to delete this board?
+      </div>
+      <div className="button-container">
+        <button
+          className="confirmation-button"
+          onClick={() => deleteBoardClick()}
+        >
+          Yes Delete Board!
+        </button>
+      </div>
+      <div>
+        <button className="cancel-button" onClick={() => cancelSubmit()}>
+          No Keep Board!
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default DeleteBoard;
--- react-app/src/components/DropdownMenuButton/index.js
+++ react-app/src/components/DropdownMenuButton/index.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect, useRef } from 'react'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import OpenModalMenuItem from '../OpenModalMenuItem'
-import DeleteConfirmForm from './DeleteConfirmForm'
+import DeleteBoard from '../DeleteBoard'
 import './DropdownMenuButton.css'
 
 function DropdownMenuButton({board}){
@@ -47,7 +47,7 @@ function DropdownMenuButton({board}){
              )}
             <ul className={showMenu ? 'dropdown-menu' : 'hidden'} ref={ulRef}>
                 <OpenModalMenuItem itemText='Edit' onItemClick={closeMenu} modalComponent={<h1>Edit Board Modal Component Goes Here</h1>} />
-                <OpenModalMenuItem itemText='Delete' onItemClick={closeMenu} modalComponent={<DeleteConfirmForm board={board} />} />
+                <OpenModalMenuItem itemText='Delete' onItemClick={closeMenu} modalComponent={<DeleteBoard board={board} />} />
             </ul>
         </div>
     )
