9ed524ed44fc248e792893865249aa33b4a2846c
tengzhong luo <108156588+tenginro@users.noreply.github.com>
2023-05-16 16:13:16 -0400

    remove prop in allpins

--- react-app/src/components/AllPins/index.js
+++ react-app/src/components/AllPins/index.js
@@ -8,7 +8,7 @@ import "./AllPins.css";
 import { NavLink } from "react-router-dom";
 import NotFound from "../NotFound";
 
-function AllPins({ searchQuery }) {
+function AllPins() {
   const dispatch = useDispatch();
   const pinsObj = useSelector((state) => state.pins.allPins);
   const user = useSelector((state) => state.session.user);
