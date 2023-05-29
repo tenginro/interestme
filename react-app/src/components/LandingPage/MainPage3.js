967a92470c46acdb1fe38c815c5e830da1deaf78
tengzhong luo <108156588+tenginro@users.noreply.github.com>
2023-05-29 13:41:38 +0800

    update landing page

--- react-app/src/components/LandingPage/MainPage3.js
+++ react-app/src/components/LandingPage/MainPage3.js
@@ -1,6 +1,6 @@
 import SignupFormModal from "../SignupFormModal";
 import image3 from "./Assets/mainpage3.jpeg";
-// import SignupFormPage from '../SignupFormPage';
+
 const MainPage3 = () => {
   const myStyle = {
     backgroundImage: `url(${image3})`,
@@ -8,9 +8,15 @@ const MainPage3 = () => {
     width: "100%",
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
   };
   return (
     <div className="main-page3-container" style={myStyle}>
+      <div style={{ fontSize: "70px", fontWeight: "600", marginLeft: "30px" }}>
+        <h2>Sign up to get your ideas</h2>
+      </div>
       <div className="signupPartInLanding">
         <SignupFormModal />
       </div>
